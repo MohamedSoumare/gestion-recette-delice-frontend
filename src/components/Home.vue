9ce1f40c4017be5585f6947df49027e968e9f0c7
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-09-03 10:36:49 +0200

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid p-5">
+    <h1 class="text-center">Bienvenue dans la cuisine de Simplon Mauritanie</h1>
+    <router-link to="/recette-list" class="btn btn-secondary">Découvrir</router-link>
+  </div>
+  
+
+  
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.container-fluid {
+  background-image: url('../assets/backgroud.jpg');
+  height: 100vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+import router from '@router'
+
+
+const app =createApp(App)
+app.component("font-awesome-icon", FontAwesomeIcon);
+const pinia = createPinia()
+app.use(pinia)
+app.use(router)
+app.mount('#app')
