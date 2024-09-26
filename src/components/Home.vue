51397bd9ed53a569f7fa5bfe4d93110f27da6dbd
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-09-27 01:18:30 +0200

    update file home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="container-fluid p-5">
     <h1 class="text-center">Bienvenue dans la cuisine de Simplon Mauritanie</h1>
     <router-link to="/recette-list" class="btn btn-secondary">Découvrir</router-link>
@@ -18,4 +18,25 @@
   background-size: cover;
   background-repeat: no-repeat;
 }
+</style> -->
+
+
+<template>
+  <div class="container-fluid p-5">
+    <h1 class="text-center">{{ $t('welcome') }}</h1>
+    <router-link to="/recette-list" class="btn btn-secondary">{{ $t('discover') }}</router-link>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.container-fluid {
+  background-image: url('../assets/backgroud.jpg');
+  height: 100vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
 </style>
+
